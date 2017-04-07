c1223a54ee6f0200db233f7d172b33c528ec6c6c
nzq <749961929@qq.com>
2017-04-07 17:57:28 +0800

    前端首页页面编写

--- Application/Runtime/Cache/Mobile/b5bbc2774616647d2eecc9fe44b263ed.php
+++ Application/Runtime/Cache/Mobile/b5bbc2774616647d2eecc9fe44b263ed.php
@@ -8,6 +8,7 @@
 	<meta http-equiv="keywords" content="关键词">
 	<meta name="description" content="描述">
 	<meta name="viewport" content="initial-scale=1.0,maximum-scale=1.0,user-scalable=0">
+
 	<link rel="stylesheet" type="text/css" href="/Template/mobile/new/Static/css/public.css" >
 	<link rel="stylesheet" type="text/css" href="/Template/mobile/new/Static/css/index.css">
 	<!-- 引入js-->
@@ -64,12 +65,171 @@
 				autoPlay:true //自动播放
 			});
 		</script>
+<!--搜索框-->
+		<div class="fake-search">
+			<div class="index_search_mid">
+				<span><img src="/Template/mobile/new/Static/images/xin/icosousuo.png" alt=""></span>
+				<input type="text" id="search_text" class="search_text" value="请输入您所搜索的商品" />
+			</div>
+		</div>
+<!--搜索框结束-->
+		<!--导航按钮-->
+		<div class="entry-list">
+			<nav>
+				<ul>
+					<li><a href=""><img src="/Template/mobile/new/Static/images/1440437165699930301.png" alt="">全部分类</a></li>
+					<li><a href=""><img src="/Template/mobile/new/Static/images/1440439318451279676.png" alt="">团购</a></li>
+					<li><a href=""><img src="/Template/mobile/new/Static/images/1440439353048484531.png" alt="">购物车</a></li>
+					<li><a href=""><img src="/Template/mobile/new/Static/images/1440439367001464442.png" alt="">个人中心</a></li>
+				</ul>
+			</nav>
+		</div>
+		<!--导航按钮结束-->
+		<!--品牌特卖-->
+		<div class="floor_images">
+			<dl>
+				<dt><a href=""><img src="/Public/upload/ad/577925b630271.jpg" alt=""></a></dt>
+				<dd>
+					<span class="Edge"><a href=""><img src="/Public/upload/ad/img2.jpg" alt=""></a></span>
+					<span><a href=""><img src="/Public/upload/ad/img3.jpg" alt=""></a></span>
+				</dd>
+			</dl>
+			<ul>
+				<li class="brom"><a href=""><img src="/Public/upload/ad/img5.jpg" alt=""></a></li>
+				<li><a href=""><img src="/Public/upload/ad/img4.jpg" alt=""></a></li>
+			</ul>
+		</div>
 
+		<!--品牌特卖结束-->
+		<!--促销商品-->
+
+		<section class="index_floor_lou">
+			<div class="floor_body">
+				<h2><em></em>限时抢购</h2>
+				<div class="scroll_promotion">
+					<ul>
+						<li>
+							<a href=""></a>
+							<div class="index_pro">
+								<a href="">
+									<div class="products_kuang">
+										<div class="timerBox">活动还未开始</div>
+										<img src="/Public/upload/xsqg/img3.jpeg" alt=""  />
+									</div>
+									<div class="good_name">金鸡送福</div>
+								</a>
+								<div class="price">
+									<a href="">
+										<img src="/Template/mobile/new/Static/images/index_flow.png" alt="">
+									</a>
+									<span class="price_pro">￥600元</span>
+								</div>
+							</div>
+						</li>
+						<li>
+							<a href=""></a>
+							<div class="index_pro">
+								<a href="">
+									<div class="products_kuang">
+										<div class="timerBox">活动已经结束</div>
+										<img src="/Public/upload/xsqg/img1.jpeg" alt=""  />
+									</div>
+									<div class="good_name">小米旗舰店正品手机平板通用迷你充电宝 移动电源10000毫安大容量</div>
+								</a>
+								<div class="price">
+									<a href="">
+										<img src="/Template/mobile/new/Static/images/index_flow.png" alt="">
+									</a>
+									<span class="price_pro">￥600元</span>
+								</div>
+							</div>
+						</li>
+						<li>
+							<a href=""></a>
+							<div class="index_pro">
+								<a href="">
+									<div class="products_kuang">
+										<div class="timerBox">活动已经结束</div>
+										<img src="/Public/upload/xsqg/img2.jpeg" alt=""  />
+									</div>
+									<div class="good_name">三星 Galaxy A9高配版 (A9100) 精灵黑 全网通4G手机 双卡双待</div>
+								</a>
+								<div class="price">
+									<a href="">
+										<img src="/Template/mobile/new/Static/images/index_flow.png" alt="">
+									</a>
+									<span class="price_pro" >￥600元</span>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+		<!--促销商品结束-->
+
+		<div class="floor_images">
+			<dl>
+				<dt><a href=""><img src="/Public/upload/ad/577925b630271.jpg" alt=""></a></dt>
+				<dd>
+					<span class="Edge"><a href=""><img src="/Public/upload/ad/img2.jpg" alt=""></a></span>
+					<span><a href=""><img src="/Public/upload/ad/img3.jpg" alt=""></a></span>
+				</dd>
+			</dl>
+		</div>
+		<!--精品推荐-->
+		<section class="index_floor">
+			<div class="floor_body1">
+				<h2></h2>
+				<div class="scroll_best">
+					<div class="bd">
+						<ul>
+							<li>
+								<div class="index_pro"><a href=""></a></div>
+							</li>
+							<li>
+								<div class="index_pro"><a href=""></a></div>
+							</li>
+							<li>
+								<div class="index_pro"><a href=""></a></div>
+							</li>
+						</ul>
+						<ul>
+							<li>
+								<div class="index_pro"><a href=""></a></div>
+							</li>
+							<li>
+								<div class="index_pro"><a href=""></a></div>
+							</li>
+							<li>
+								<div class="index_pro"><a href=""></a></div>
+							</li>
+						</ul>
+						<ul>
+							<li>
+								<div class="index_pro"><a href=""></a></div>
+							</li>
+							<li>
+								<div class="index_pro"><a href=""></a></div>
+							</li>
+							<li>
+								<div class="index_pro"><a href=""></a></div>
+							</li>
+						</ul>
+						<div class="hd"></div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--精品推荐结束-->
 
 
 	</div>
 
 </div>
+
+
+
 <style>
 .ul_test{padding: 0; margin: 0; list-style-type: none;}
 .ul_test li{display: inline-block; border: 1px solid #000;}
